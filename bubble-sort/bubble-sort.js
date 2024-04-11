ee1de51cbf928b0090e427d60e3971a03fd0f8f9
coreyisaiah <cmorton205@gmail.com>
2024-04-11 08:52:15 -0500

    completed bubble-sort

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -1,8 +1,18 @@
 
 function bubbleSort(arr) {
-
+  let swapped
+  do{
+    swapped = false
+    for (let i = 0; i < arr.length -1; i++){
+      if (arr[i] > arr[i+1]){
+        let temp =  arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+        swapped = true
+      }
+    }
+  }while(swapped)
     // Iterate through the array
-
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
