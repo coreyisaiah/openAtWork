a52f01ad9021ec6c6d1ff2ee068e43179767aaf1
corey <cmorton205@gmail.com>
2024-07-07 17:55:12 -0500

    completes all of the remastery materials

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,22 @@
-let obj = { name: "Crystal", traits: ["mammal", "dog"] };
-let { age } = obj;
+class Dog {
+    constructor(name, isSitting){
+        this.name = name
+        this.isSitting = isSitting
+    }
 
-console.log(age)
+    stand(){
+        this.isSitting = false;
+        return `${this.name} is now standing`
+    }
+}
+
+let dog = new Dog("Bowser", true);
+
+// Bowser starts out sitting
+console.log(dog.isSitting); // prints `true`
+
+// // Let's make him stand
+ console.log(dog.stand()); // prints `false`
+
+// // He's actually standing now!
+console.log(dog.isSitting); // prints `false`
